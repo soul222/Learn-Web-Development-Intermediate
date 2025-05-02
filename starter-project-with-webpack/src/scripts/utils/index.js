ee6a7ed3d02d26c385f025458dc407f415207875
soultan amirul mukminin <soultanamirul222@gmail.com>
2025-05-03 01:14:56 +0700

    Add files via upload

---
+++ starter-project-with-webpack/src/scripts/utils/index.js
@@ -0,0 +1,94 @@
+export function showFormattedDate(date, locale = 'en-US', options = {}) {
+  return new Date(date).toLocaleDateString(locale, {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    ...options,
+  });
+}
+
+export function convertBase64ToBlob(base64Data, contentType = '', sliceSize = 512) {
+  const byteCharacters = atob(base64Data);
+  const byteArrays = [];
+
+  for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+    const slice = byteCharacters.slice(offset, offset + sliceSize);
+
+    const byteNumbers = new Array(slice.length);
+    for (let i = 0; i < slice.length; i++) {
+      byteNumbers[i] = slice.charCodeAt(i);
+    }
+
+    const byteArray = new Uint8Array(byteNumbers);
+    byteArrays.push(byteArray);
+  }
+
+  return new Blob(byteArrays, { type: contentType });
+}
+
+export function convertBase64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
+  const rawData = atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; i++) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
+export function setupSkipToContent(element, mainContent) {
+  element.addEventListener('click', () => mainContent.focus());
+}
+
+export function transitionHelper({ skipTransition = false, updateDOM }) {
+  if (skipTransition || !document.startViewTransition) {
+    const updateCallbackDone = Promise.resolve(updateDOM()).then(() => {});
+
+    return {
+      ready: Promise.reject(Error('View transition unsupported.')),
+      updateCallbackDone,
+      finished: updateCallbackDone
+    };
+  }
+
+  return  document.startViewTransition(updateDOM);
+}
+
+export function sleep(time = 1000) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+export function isServiceWorkerAvailable() {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register('/sw.bundle.js')
+        .then((registration) => {
+          console.log('Service Worker registered successfully:', registration);
+        })
+        .catch((error) => {
+          console.log('Registration Service Worker failed:', error);
+        });
+    });
+  }
+  
+  return 'serviceWorker' in navigator;
+}
+
+export async function registerServiceWorker() {
+  if (!isServiceWorkerAvailable()) {
+    console.log('Service worker API unsupported.');
+    return;
+  }
+
+  try {
+    const registration = await navigator.serviceWorker.register('/sw.bundle.js');
+    console.log('service worker has been installed', registration);
+  } catch (error) {
+    console.log('Failed to install service worker:', error);
+  }
+}
+
+
